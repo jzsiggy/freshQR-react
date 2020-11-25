7513cc8aaa906d335d3b2d095fc34bd0720b98e5
Joao Zsigmond <jzsiggy@gmail.com>
2020-11-24 21:25:19 -0300

    setting endpoint

--- src/assets/API.js
+++ src/assets/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://142.93.127.120:5000/api',
+    baseURL: 'https://api.freshqr.io/api',
     // baseURL: 'http://localhost:5000/api',
     withCredentials: true
 });
