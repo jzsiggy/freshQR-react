3d7688e63b91369408cac78b0e518b63f3aa71a4
Joao Zsigmond <jzsiggy@gmail.com>
2023-01-23 03:36:58 -0300

    adding filters

--- src/components/dashboard/list/List.js
+++ src/components/dashboard/list/List.js
@@ -1,13 +1,27 @@
 import React , { Component } from 'react';
 import { Wrapper } from './styles';
+import { Input } from '../../form/styles';
 import Card from './card/Card';
 
 class List extends Component {
+    state = {
+        filter: '',
+    }
+
+    handleSetFilter = (e) => {
+        this.setState({
+            content: e.target.value
+        });
+    };
+
     render() {
         return (
             <Wrapper>
+            <Input name='filter' value={this.state.filter} placeholder='filtro' onChange={(e) => this.handleSetFilter(e)}/>
             {
-                this.props.codes.map((code, index) => {
+                this.props.codes
+                .filter(code => code.name.includes(this.state.filter))
+                .map((code, index) => {
                     return (
                         <Card 
                         key={index} 
